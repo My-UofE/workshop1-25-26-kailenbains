58cfed167b34e81b59b9b74c5d8faeeb633af969
kailenbains <kkkb201@exeter.ac.uk>
2026-01-20 17:08:51 +0000

    Game 3 Function written

--- game_functions.py
+++ game_functions.py
@@ -16,21 +16,19 @@ def check_higher_lower(current_val, next_val, user_input):
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    if letter not in word:
-        print(f"Sorry, '{letter}' is not in the word")
-        return False
-    else:
-        word_list = []
-        index_list = []
-        for character in word:
-            word_list.append(character)
-        for item in word:
-            if letter in word:
-                letter_index = word_list.index(letter)
-                index_list.append(letter_index)
-        for index in index_list:
-            board[index] = letter
+    found = False
+
+    # check each letter in the word
+    for i in range(len(word)):
+        if word[i] == letter:
+            board[i] = letter
+            found = True
+
+    if found:
         print(f"Well done! '{letter}' is in the word")
         return True
+    else:
+        print(f"Sorry, '{letter}' is not in the word")
+        return False  
 
 
